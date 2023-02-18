05b2aec47fcc33af50f465cbc513af6fcddd12fc
Aishayakubu <aishayakubu377@gmail.com>
2023-02-18 11:49:53 -0800

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+/**
+ * main-using put function
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	puts('\"Programming is like building multilingual puzzle\n");
-	return(0);
+puts("\"Programming is like building multilingual puzzle");
+
+return (0);
 }
