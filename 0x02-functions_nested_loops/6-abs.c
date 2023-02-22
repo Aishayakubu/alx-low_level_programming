5d741e93ae3f7ec6b1ce2d2d92533897236c4721
Aishayakubu <aishayakubu377@gmail.com>
2023-02-22 12:42:29 -0800

    modified

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,17 @@
  *_abs - entry point
  *
  * @n: character to be checked
- * Return: n if greater than 0, 0 if not
+ * Return: 1 if positive, -1 if negative, 0 if otherwise
  */
 int _abs(int n)
 {
 if (n > 0)
 {
-return (n);
+return (1);
+}
+else if (n < 0)
+{
+return (-1);
 }
 else
 {
