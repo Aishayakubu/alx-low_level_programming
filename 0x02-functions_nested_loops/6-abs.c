38e3e19a82ff9ce2a9d1c3cacf35af9c031d1bab
Aishayakubu <aishayakubu377@gmail.com>
2023-02-22 12:34:13 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - entry point
+ *
+ * @n: character to be checked
+ * Return: n if greater than 0, 0 if not
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (0);
+}
+}
