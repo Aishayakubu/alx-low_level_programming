5ecb51ffe1c4301183b9354c7172802ac6d97010
Aishayakubu <aishayakubu377@gmail.com>
2023-02-26 12:02:20 -0800

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * @n: character to be checked
+ * return: void
+ *
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+{
+_putchar('n');
+_putchar(',');
+}
+}
