57dd0d68c186d0ae67b28f2663736ba246b88d32
Aishayakubu <aishayakubu377@gmail.com>
2023-02-27 08:53:36 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main - entry point
+ *
+ *Return: (0)
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a'; i < 'z' + 1; i++)
+{
+_putchar('i');
+}
+_putchar('\n');
+return (0);
+}
