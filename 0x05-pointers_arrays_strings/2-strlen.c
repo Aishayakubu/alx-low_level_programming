518388ee81eeb6eed363362d1105b8ba2e8b4437
Aishayakubu <aishayakubu377@gmail.com>
2023-02-28 17:10:31 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - entry point
+ * @s: character to be checked
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int a = 0;
+if (*s != '0')
+{
+a++;
+s++;
+}
+
+return (0);
+}
