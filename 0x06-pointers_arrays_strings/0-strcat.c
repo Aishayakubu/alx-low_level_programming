8c6467ccf07bf2ed7dd8b4980fdedd352fbe4545
Aishayakubu <aishayakubu377@gmail.com>
2023-03-02 14:04:15 -0800

    readme

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatenates two string
+ * @dest: first string
+ * @src: second string
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src);
+{
+char dest;
+char src;
+int i;
+for(i = 0;i
+}
