2e163e2d42cd65a73c65fa3bed1ba0c7e095e376
Aishayakubu <Aishayakubu377@gmail.com>
2023-07-16 07:22:50 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of a program that prints a line with the print function
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
