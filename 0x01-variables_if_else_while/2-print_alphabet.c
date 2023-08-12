795fbe1e5e906e281bac75b36c42853a3cf5414c
Aishayakubu <Aishayakubu377@gmail.com>
2023-08-12 01:11:50 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description@ A program that the lowercase alphabets
+ * Return: 0 success
+ */
+int main(void)
+{
+char i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
