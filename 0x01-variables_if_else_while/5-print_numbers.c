d9a2701f753d0526d127f78a2b35b50dbf45f679
Aishayakubu <Aishayakubu377@gmail.com>
2023-08-12 15:57:46 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description@  print single numbers
+ * Return:  0 if successful
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+putchar('\n');
+return (0);
+}
