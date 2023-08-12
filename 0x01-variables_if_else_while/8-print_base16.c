dc2f5555588a527c9d36ad2267f4c8e51e2ca10a
Aishayakubu <Aishayakubu377@gmail.com>
2023-08-12 16:51:34 -0700

    alphanum

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description@ A program that prints numbers of base 10
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int i;
+char j;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (j = 97; j < 102; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
