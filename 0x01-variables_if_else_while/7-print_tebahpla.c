f58a366ac16fe4edd47b2edb2a8edde20e66414d
Aishayakubu <Aishayakubu377@gmail.com>
2023-08-12 16:28:03 -0700

    reversed alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description@ A program that prints lowercase alphabets in reverse
+ * Return: 0 if successful
+ */
+int main(void)
+{
+char i;
+for (i = 122; i < 98; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
