78919af7141f0454ecb35edb26b1b5f3a3468448
Aishayakubu <Aishayakubu377@gmail.com>
2023-08-15 10:31:14 -0700

    check if lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Description @ check if alphabet is lowercase
+ * Return: 1 if successful otherwise 0
+ */
+int _islower(int c)
+{
+if ('a' <= c && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
