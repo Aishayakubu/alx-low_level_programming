008fc0ef2abeae62c88e71b595106ffa38aeebb5
Aishayakubu <Aishayakubu377@gmail.com>
2023-08-19 17:09:29 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: character to be checked
+ * @b: character to be checked
+ * Description@ multiplication
+ * Return: multiplicaton of two numbers
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
